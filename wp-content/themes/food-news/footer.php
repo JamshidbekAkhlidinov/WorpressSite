cf992eead1ab0a6d2d1f7e86ea6ce96d32d34a61
Jamshidbek Axlidinov <jamshidbekaxlidinovtatu@gmail.com>
2022-05-06 16:40:57 +0500

    install themes

---
+++ wp-content/themes/food-news/footer.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+* The template for displaying the footer.
+*
+* Contains the closing of the #content div and all content after
+*
+* @package Food News
+*/
+?>
+
+
+<footer class="main">
+	<div class="container">
+		<?php dynamic_sidebar( 'footer-1' ); ?>
+	</div>
+</footer>
+<div class="copyright text-center">
+	<?php esc_html_e( "Powered by", 'food-news' );?> <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'food-news' ) );
+	?>"><?php esc_html_e( "WordPress", 'food-news' );
+	?></a> | <a href="<?php echo esc_url( 'https://wpmagplus.com/' ) ;
+	?>" target="_blank"  rel="nofollow"><?php 
+	esc_html_e( 'Food News by WP Mag Plus', 'food-news' );
+?></a>
+<?php 
+?>
+</div>
+<div class="scroll-top-wrapper"> <span class="scroll-top-inner"><i class="fa fa-2x fa-angle-up"></i></span></div>
+
+
+<?php 
+wp_footer();
+?>
+</body>
+</html>
